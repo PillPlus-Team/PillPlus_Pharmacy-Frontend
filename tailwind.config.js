9ac00492289dd076ed781049e4a4c07e6960924f
EsitoneX <phoom0529@gmail.com>
2021-04-19 17:46:49 +0700

    🔧 Update scaling in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
     purge: ['./src/**/*.html', './src/**/*.js'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                112: '28rem',
+                128: '32rem',
+                144: '36rem',
+                160: '40rem',
+                176: '44rem',
+            },
+        },
     },
     variants: {
-        extend: {},
+        extend: {
+            backgroundColor: ['active'],
+            textColor: ['active'],
+        },
     },
     plugins: [],
 };
